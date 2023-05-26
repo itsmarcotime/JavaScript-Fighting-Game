9672d9849c09624932b9a35989fe90923848e3a8
Marco <patrickstar1281@yahoo.com>
2023-05-26 09:00:07 -0500

    added player movement

--- scripts.js
+++ scripts.js
@@ -21,6 +21,7 @@ class Sprite {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity.y = 0;
@@ -52,11 +53,49 @@ const enemy = new Sprite({
   },
 });
 
+
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+  if (keys.a.pressed) {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed) {
+    player.velocity.x = 1  
+  };
 };
 animate();
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+  }
+});
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
