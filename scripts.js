09ced42aacbe07e09f54bcb8a31facde32ec38ea
Marco <patrickstar1281@yahoo.com>
2023-06-14 05:54:09 -0500

    movement has been set up

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2;
+const gravity = 0.7;
 
 class Sprite {
   constructor({ position, velocity }) {
@@ -75,7 +75,6 @@ const keys = {
     pressed: false
   }
 };
-let lastKey
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -88,17 +87,17 @@ function animate() {
   enemy.velocity.x = 0;
 
   // player movement
-  if (keys.a.pressed && lastKey === 'a') {
-    player.velocity.x = -1;
-  } else if (keys.d.pressed && lastKey === 'd') {
-    player.velocity.x = 1  
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5;
   };
 
   // enemy movement
   if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-    enemy.velocity.x = -1;
+    enemy.velocity.x = -5;
   } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-    enemy.velocity.x = 1  
+    enemy.velocity.x = 5;
   };
 };
 animate();
@@ -108,14 +107,14 @@ window.addEventListener('keydown', (e) => {
   switch (e.key) {
     case "d":
       keys.d.pressed = true;
-      lastKey = "d";
+      player.lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
-      lastKey = "a";
+      player.lastKey = "a";
       break;
     case "w":
-      player.velocity.y = -10;
+      player.velocity.y = -20;
       break;
     case "ArrowRight":
       keys.ArrowRight.pressed = true;
@@ -126,7 +125,7 @@ window.addEventListener('keydown', (e) => {
       enemy.lastKey = "ArrowLeft"
       break;
     case "ArrowUp":
-      player.velocity.y = -10;
+      enemy.velocity.y = -20;
       break;
   }
 });
