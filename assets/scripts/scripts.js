e6feb440517b9258e5df5b1e58612409ff0a32df
Marco <patrickstar1281@yahoo.com>
2023-06-23 10:34:28 -0500

    added background sprite

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,6 +7,14 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 
+const background = new Sprite ({
+  position: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: './assets/images/background.png'
+})
+
 const player = new Fighter({
   position: {
     x: 0,
@@ -61,48 +69,14 @@ const keys = {
   }
 };
 
-function collisionDetection({ rectangle1, rectangle2 }) {
-  return (
-   rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
-   rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-   rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
-   rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
-  );
-};
-
-function determineWinner({player, enemy, timerId }) {
-  clearTimeout(timerId);
-  document.querySelector('#tie').style.display = "flex";
-
-  if (player.health === enemy.health) {
-    document.querySelector('#tie').innerHTML = "Tie";
-  } else if (player.health > enemy.health) {
-    document.querySelector('#tie').innerHTML = "Player 1 Wins!";
-  } else if (enemy.health > player.health) {
-    document.querySelector('#tie').innerHTML = "Player 2 Wins!";
-  }
-}
-
-let timer = 60;
-let timerId;
-
-function decreaseTimer() {
-  if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000);
-    timer--;
-    document.querySelector('#timer').innerHTML = timer;
-  };
-
-  if (timer === 0) {
-    determineWinner({player, enemy, timerId });
-  }
-};
 decreaseTimer();
 
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  background.update();
   player.update();
   enemy.update();
 
